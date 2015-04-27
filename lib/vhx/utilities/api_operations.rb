84eee9d825c862d64cfe4dc9867a4cffb6d84d82
Sagar Shah <sgrshah@gmail.com>
2015-04-27 17:22:24 -0400

    Allow search by hypermedia or id. Whitelist create to certain models.

--- lib/vhx/utilities/api_operations.rb
+++ lib/vhx/utilities/api_operations.rb
@@ -1,8 +1,16 @@
 module Vhx
   module ApiOperations
-    def find(resource)
-      response_json = Vhx.connection.get(resource).body
-      self.new(response_json)
+    def find(identifier)
+      klass = get_klass
+
+      if identifier.class.to_s == 'String'
+        hypermedia = identifier
+      elsif identifier.class.to_s.match(/Integer|Fixnum/)
+        hypermedia = '/' + klass.downcase.pluralize + '/' + identifier.to_s #This url is based purely on VHX's API convention (not nested).
+      end
+
+      response = Vhx.connection.get(hypermedia)
+      self.new(response.body)
     end
 
     def all(resource)
@@ -11,12 +19,24 @@ module Vhx
     end
 
     def create(options)
+      klass = get_klass
+
+      unless ['Site', 'Ticket', 'Package', 'Video'].include?(klass)
+        raise Error
+      end
+
       response = Vhx.connection.post do |req|
-        req.url("/" + self.to_s.split("::").last.downcase.pluralize) #This url is based purely on VHX's API convention.
-        req.body =  options
+        req.url('/' + klass.downcase.pluralize) #This url is based purely on VHX's API convention.
+        req.body = options
       end
 
       self.new(response.body)
+    end
+
+protected
+
+    def get_klass
+      self.to_s.split("::").last
     end
 
   end
