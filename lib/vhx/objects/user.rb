8af26d9b32078b943c48271e043a2ef8f4bb910c
Sagar Shah <sgrshah@gmail.com>
2015-04-27 17:22:57 -0400

    Add User#update method.

--- lib/vhx/objects/user.rb
+++ lib/vhx/objects/user.rb
@@ -7,5 +7,10 @@ module Vhx
       response_json = Vhx.connection.get('/me').body
       self.new(response_json)
     end
+
+    def update(options)
+      options = {id: self.id}.merge(options)
+      Vhx.connection.put('/settings', options)
+    end
   end
 end
