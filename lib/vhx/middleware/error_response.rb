56248a3d7e72ce1901d0eb5393af5619c31115a4
Sagar Shah <sgrshah@gmail.com>
2015-05-01 14:37:44 -0400

    Use fetch for 401 errors.

--- lib/vhx/middleware/error_response.rb
+++ lib/vhx/middleware/error_response.rb
@@ -8,7 +8,7 @@ module Vhx
         when 400
           BadRequestError
         when 401
-          if env[:body]['message'].match(/token/)
+          if env[:body].fetch('message', '').match(/token/)
             InvalidTokenError
           else
             UnauthorizedError
