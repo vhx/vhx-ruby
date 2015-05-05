0af32df49b81df1d57292c60699b551e0529ad78
Sagar Shah <sgrshah@gmail.com>
2015-05-05 14:41:54 -0400

    Specs for VhxObject

--- spec/test_data.rb
+++ spec/test_data.rb
@@ -4,9 +4,8 @@ module TestData
       client_id: "34ac1dec21fc20afd5099a2e6e0e62745dead7acd20c7b87320d8e5a7c5a33cc",
       client_secret: "6a04e2cf039db7ff57850f677fe0e419cdc132b3cc04aac9f3c2cd1c193a326d",
       oauth_token: {
-        access_token:  "41d2ed8ead125347d38cf13dd7da8df0a32e9c9e839a9ea1b39c0d4f8b9caa92",
-        refresh_token: "3483852aa88be2632faa47486fb8aa44d331aa301b696015338de7a6b5cb7025",
-        expires_at: 1430773600,
+        access_token:  "cc7d6c93bde201968f077b73acd5ee11a2768cbcb9b43122eb294004bf5b4480",
+        refresh_token: "3b9aacbeb03d05e7b901c35580bcc45e6e714cd4921d8bc8699342632f68bd63",
         expires_in: 7200
       }
     }
--- spec/utilities/vhx_object_spec.rb
+++ spec/utilities/vhx_object_spec.rb
@@ -1,19 +1,53 @@
 require 'spec_helper'
 
-describe Vhx::VhxObject do
-  let(:sample_user_response){ JSON.parse(File.read("spec/fixtures/sample_user_response.json")) }
+describe Vhx::VhxObject, :vcr do
+  let(:sample_package_response){ JSON.parse(File.read("spec/fixtures/sample_package_response.json")) }
+  let(:vhx_object_with_embedded){Vhx::Package.new(sample_package_response)}
+  let(:vhx_object_without_embedded){sample_package_response['_embedded'] = {}; Vhx::Package.new(sample_package_response);}
 
   describe '#validate_class' do
     it 'raises error' do
-      expect{Vhx::VhxObject.new(sample_user_response)}.to raise_error(Vhx::InvalidResourceError)
+      expect{Vhx::VhxObject.new(sample_package_response)}.to raise_error(Vhx::InvalidResourceError)
     end
 
     it 'succeeds' do
-      expect(Vhx::User.new(sample_user_response)).to be_instance_of(Vhx::User)
+      expect(Vhx::Package.new(sample_package_response)).to be_instance_of(Vhx::Package)
     end
   end
 
-  describe '#create_accessors' do
-    expect(Vhx::User.new(sample_user_response)).to be_instance_of(Vhx::User)
+  describe 'associations' do
+    before {
+      Vhx.setup(test_credentials)
+    }
+
+    it 'default to embedded' do
+      Faraday::Connection.any_instance.should_not_receive(:get)
+      expect(vhx_object_with_embedded.site).to be_instance_of Vhx::Site
+    end
+
+    it 'falls back to links' do
+      Faraday::Connection.any_instance.should_receive(:get).and_call_original
+      expect(vhx_object_without_embedded.site).to be_instance_of Vhx::Site
+    end
+
+    describe 'cache' do
+      it 'retreive if available' do
+        vhx_object_without_embedded.site
+        Faraday::Connection.any_instance.should_not_receive(:get)
+        expect(vhx_object_without_embedded.site).to be_instance_of Vhx::Site
+      end
+    end
+
+    describe 'has_many' do
+      it 'returns VhxCollection object' do
+        expect(vhx_object_with_embedded.videos).to be_instance_of Vhx::VhxCollection
+      end
+    end
+
+    describe 'has_one' do
+      it 'returns resource object' do
+        expect(vhx_object_with_embedded.site).to be_instance_of Vhx::Site
+      end
+    end
   end
 end
