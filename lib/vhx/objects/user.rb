632c2bf35ebc8556913838eacb70eedbe4d251d5
Sagar Shah <sgrshah@gmail.com>
2015-10-22 13:55:15 -0400

    add selling_packages method to user

--- lib/vhx/objects/user.rb
+++ lib/vhx/objects/user.rb
@@ -11,5 +11,21 @@ module Vhx
       options = {id: self.id}.merge(options)
       Vhx.connection.put('/settings', options)
     end
+
+    def selling_packages
+      if @selling_packages.present?
+        return @selling_packages
+      end
+
+      @selling_packages = []
+
+      @obj_hash['_embedded']['sites'].each do |site|
+        site = Vhx.connection.get(site['_links']['self']['href'])
+        @selling_packages << site['_embedded']['packages']
+      end
+
+      build_association(@selling_packages, 'packages')
+    end
+
   end
 end
