795512fb4099735d4c6959cbffb16c56bea37dd4
Sagar Shah <sgrshah@gmail.com>
2016-01-14 17:51:08 -0500

    Fix list payload. Up Version

--- lib/vhx/utilities/api_operations/list.rb
+++ lib/vhx/utilities/api_operations/list.rb
@@ -5,10 +5,7 @@ module Vhx
     module List
       module ClassMethods
         def all(payload = {})
-          response = Vhx.connection.get do |req|
-            req.url '/' + get_klass.downcase + 's'
-            req.body = payload
-          end
+          response = Vhx.connection.get('/' + get_klass.downcase + 's', payload)
           VhxListObject.new(response.body, get_klass.downcase + 's')
         end
       end
--- lib/vhx/version.rb
+++ lib/vhx/version.rb
@@ -1,3 +1,3 @@
 module Vhx
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
