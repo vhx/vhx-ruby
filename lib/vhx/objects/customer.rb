bc1e886c9f663eb5ea1cebf1714ebe20d2fff394
Sagar Shah <sgrshah@gmail.com>
2016-02-16 16:02:24 -0500

    add and remove products to customers.

--- lib/vhx/objects/customer.rb
+++ lib/vhx/objects/customer.rb
@@ -4,5 +4,23 @@ module Vhx
     include Vhx::ApiOperations::Request
     include Vhx::ApiOperations::List
     include Vhx::ApiOperations::Delete
+
+    def add_product(identifier)
+      response = Vhx.connection.put do |req|
+        req.url('/customers/' + self.id.to_s + '/products')
+        req.body = { product: get_hypermedia(identifier, 'Product') }
+      end
+
+      self.class.new(response.body)
+    end
+
+    def remove_product(identifier)
+      response = Vhx.connection.delete do |req|
+        req.url('/customers/' + self.id.to_s + '/products')
+        req.body = { product: get_hypermedia(identifier, 'Product') }
+      end
+
+      self.class.new(response.body)
+    end
   end
 end
