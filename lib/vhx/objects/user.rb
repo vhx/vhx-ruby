1e2ad6413bad148c90e5f63ef79c6775986e39ec
Sagar Shah <sgrshah@gmail.com>
2016-12-05 11:01:12 -0500

    remmove support for /me from user resource.

--- lib/vhx/objects/user.rb
+++ lib/vhx/objects/user.rb
@@ -1,10 +1,5 @@
 module Vhx
   class User < VhxObject
     include Vhx::ApiOperations::Request
-
-    def self.me
-      response_json = Vhx.connection.get('/me').body
-      self.new(response_json)
-    end
   end
 end
