087c7f03ef74241052eb4943805e3c6777cb3e50
Kevin Sheurs <ksheurs@gmail.com>
2017-02-06 15:16:17 -0500

    Add specs

--- spec/objects/customer_spec.rb
+++ spec/objects/customer_spec.rb
@@ -12,34 +12,34 @@ describe Vhx::Customer do
   before{
     Vhx.setup({api_key: 'testapikey'})
   }
-  
+
   describe 'api operations' do
 
     describe '::find' do
       it 'does not error' do
         Vhx.connection.stub(:get).and_return(OpenStruct.new(body: customer_response))
-        expect{Vhx::Customer.find(123)}.to_not raise_error 
+        expect{Vhx::Customer.find(123)}.to_not raise_error
       end
-    end 
+    end
 
     describe '::retrieve' do
       it 'does not error' do
         Vhx.connection.stub(:get).and_return(OpenStruct.new(body: customer_response))
-        expect{Vhx::Customer.retrieve(123)}.to_not raise_error 
+        expect{Vhx::Customer.retrieve(123)}.to_not raise_error
       end
     end
 
     describe '::list' do
       it 'does not error' do
         Vhx.connection.stub(:get).and_return(OpenStruct.new(body: customers_response))
-        expect{Vhx::Customer.list()}.to_not raise_error 
+        expect{Vhx::Customer.list()}.to_not raise_error
       end
     end
 
     describe '::all' do
       it 'does not error' do
         Vhx.connection.stub(:get).and_return(OpenStruct.new(body: customers_response))
-        expect{Vhx::Customer.all()}.to_not raise_error 
+        expect{Vhx::Customer.all()}.to_not raise_error
       end
     end
 
@@ -61,13 +61,29 @@ describe Vhx::Customer do
         Vhx.connection.stub(:delete).and_return(OpenStruct.new(body: ""))
         expect{Vhx::Customer.delete(1)}.to_not raise_error
       end
+
+      it "supports headers" do
+        params = [
+          :delete,
+          "https://api.vhx.tv/customers/1",
+          nil,
+          { "VHX-Client-IP" => "1.2.3.4" },
+        ]
+        response = OpenStruct.new(body: "")
+
+        expect_any_instance_of(Faraday::Connection).to receive(:run_request).
+                                                       with(*params).
+                                                       and_return(response)
+
+        Vhx::Customer.delete(1, {}, { 'VHX-Client-IP' => '1.2.3.4' })
+      end
     end
 
     describe '#add_product' do
       it 'returns customer' do
         Vhx.connection.stub(:put).and_return(OpenStruct.new(body: customer_response))
         customer = Vhx::Customer.new(customer_response).add_product(1)
-        expect(customer.class).to eq(Vhx::Customer) 
+        expect(customer.class).to eq(Vhx::Customer)
       end
     end
 
@@ -75,7 +91,7 @@ describe Vhx::Customer do
       it 'returns customer' do
         Vhx.connection.stub(:delete).and_return(OpenStruct.new(body: customer_response))
         customer = Vhx::Customer.new(customer_response).remove_product(1)
-        expect(customer.class).to eq(Vhx::Customer) 
+        expect(customer.class).to eq(Vhx::Customer)
       end
     end
   end
